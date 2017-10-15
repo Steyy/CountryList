2a66fcc7609530c0e7e7d185fbff3ce49169fe4e
Steyy <javi_areva@hotmail.com>
2017-10-15 20:27:55 +0200

    Insertamos y convertimos tabla

--- app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -3,11 +3,22 @@ package edu.upc.eseiaat.pma.countrylist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CountryListActivity extends AppCompatActivity {
+
+    private ArrayList<String> country_list; //contenedor secuencial, puedes mover objetos
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_list);
+
+        String[] countries = getResources().getStringArray(R.array.countries);
+        country_list = new ArrayList<>(Arrays.asList(countries)); //conversor
+
+
+
     }
 }
