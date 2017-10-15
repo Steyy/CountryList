1ac52218e1a26092157ee8f871c833aa559029e5
Steyy <javi_areva@hotmail.com>
2017-10-15 20:36:35 +0200

    Ponemos el adaptador y comprobamos que funciona

--- app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -2,12 +2,15 @@ package edu.upc.eseiaat.pma.countrylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CountryListActivity extends AppCompatActivity {
 
+    private ArrayAdapter<String> adapter;
     private ArrayList<String> country_list; //contenedor secuencial, puedes mover objetos
 
     @Override
@@ -18,7 +21,11 @@ public class CountryListActivity extends AppCompatActivity {
         String[] countries = getResources().getStringArray(R.array.countries);
         country_list = new ArrayList<>(Arrays.asList(countries)); //conversor
 
+        ListView list = (ListView) findViewById(R.id.country_list); //obtengo datos
 
+        //todos los ListViews tienen un Adapter, gestiona los peldaños de la escalera
 
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, country_list);
+        list.setAdapter(adapter);
     }
 }
